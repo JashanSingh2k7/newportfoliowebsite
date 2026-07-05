0b18b14b1ef1cd3fa39e8f8cfd24da4c7bd21775
fullstakdeveloper <pupru.edu2k7@gmail.com>
2026-07-05 14:56:36 -0400

    personal name update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import React from "react";
 import "./App.css";
-import LandingPage from "./components/landPage.js";
-import AboutMe from "./components/aboutMe.js";
-import VantaBackground from "./components/VantaBackground.js";
-import Projects from "./components/projects.js";
-import Resume from "./components/resume.js";
 
 function App() {
   return (
     <div className = "App">
-      <VantaBackground/>
-      <LandingPage />
-      <AboutMe />
-      <Projects/>
-      <Resume/>
+      Jashan
     </div>
   );
 }
